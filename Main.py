ac7f39bf502df4863edf6d6640f7dc1e44b3d056
L01ss <loufarasyn@gmail.com>
2026-03-16 21:39:24 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -5,12 +5,12 @@ import LegCalculations
 import ControllerCalculator
 
 # Creates Leg the objects from the class Leg
-Leg0 = LegCalculations.Leg(legID=0, offset=(77,131,0),  angle=-35,  lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,131,40))
-Leg1 = LegCalculations.Leg(legID=1, offset=(100,0,0),   angle=0,    lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(200,0,40))
-Leg2 = LegCalculations.Leg(legID=2, offset=(77,-131,0), angle=35,   lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,-131,40))
-Leg3 = LegCalculations.Leg(legID=3, offset=(77,131,0), angle=-35,  lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,131,40))
-Leg4 = LegCalculations.Leg(legID=4, offset=(100,0,0),   angle=0,    lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(200,0,40))
-Leg5 = LegCalculations.Leg(legID=5, offset=(77,-131,0),  angle=35,   lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,-131,40))
+Leg0 = LegCalculations.Leg(legID=0, offset=(77,131,0),  angle=-35,  lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,131,50))
+Leg1 = LegCalculations.Leg(legID=1, offset=(100,0,0),   angle=0,    lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(200,0,50))
+Leg2 = LegCalculations.Leg(legID=2, offset=(77,-131,0), angle=35,   lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,-131,50))
+Leg3 = LegCalculations.Leg(legID=3, offset=(77,131,0), angle=-35,  lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,131,50))
+Leg4 = LegCalculations.Leg(legID=4, offset=(100,0,0),   angle=0,    lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(200,0,50))
+Leg5 = LegCalculations.Leg(legID=5, offset=(77,-131,0),  angle=35,   lowerleg_length=93.2, middleleg_length=67.2, upperleg_length=40, restPosition=(170,-131,50))
 
 def stand(seconds):
     """
@@ -104,7 +104,6 @@ def gaitPatternManager(patternID :int, turnAngle, stepLength):
         ServoController.updateLegServos(5, a1, a2, a3)
 
         time.sleep(delay)
-        ServoController.detachServos()
 
 # makes the robot stand still for 3 seconds before starting
 stand(3)
